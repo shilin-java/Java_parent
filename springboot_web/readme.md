39db641c6c05b527ae35a0311d8e5aa358174cee
heqf <heqf@chujiu-data.com>
2021-02-24 16:23:00 +0800

    创建子项目：springboot_web,简单页面

---
+++ springboot_web/readme.md
@@ -0,0 +1,16 @@
+
+#参考文章
+知乎：https://www.jianshu.com/p/546e9aace657
+博客园：https://www.cnblogs.com/bigdataZJ/p/springboot-log.html
+博客园：https://www.cnblogs.com/coderxx/p/11390341.html
+#github
+https://github.com/TiantianUpup/springboot-log
+
+#实现目标
+1.市场上的日志种类
+
+2.如何选择合适的日志系统
+
+3.如何进行配置
+
+4.如何搭建简单的elk日志系统
---
+++ springboot_web/src/main/java/com/raymon/springboot_log/entity/Person.java
@@ -0,0 +1,20 @@
+package com.raymon.springboot_log.entity;
+
+/**
+ * Created in       2021/2/24 12:23
+ *
+ * @Author: Lin.Shi
+ * @version: 1.0
+ * @Description:
+ */
+public class Person {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
